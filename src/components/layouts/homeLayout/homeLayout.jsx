5ce784312e4c5727161c31f7fd38574747010c8f
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-08-08 22:53:21 +0600

    added berlin font in my project and text route added in sidebar

--- src/components/layouts/homeLayout/homeLayout.jsx
+++ src/components/layouts/homeLayout/homeLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../navbar/navbar';
 
 const HomeLayout = ({children}) => {
     return (
-        <div className="grid grid-cols-10">
+        <div className="grid grid-cols-10 h-screen">
             <div className="col-start-1 col-end-2 bg-gradient-to-b from-[#3D2D3B] to-[#232141]">
                 <Sidebar></Sidebar>
             </div>
