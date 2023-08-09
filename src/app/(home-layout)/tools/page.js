d14c42ec025af28eae0983008c4bcaf516a2ecf6
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-08-09 17:11:12 +0600

    complete tools page desktop size design

--- src/app/(home-layout)/tools/page.js
+++ src/app/(home-layout)/tools/page.js
@@ -1,9 +1,74 @@
 import ImageCard from "@/components/shared/ImageCard/ImageCard";
 import imageCardInfo from "@/temp-data/imageCardInfo";
+import Image from "next/image";
+import { BiSolidChevronsLeft, BiSolidChevronsRight } from "react-icons/bi";
 const Tools = () => {
   return (
-    <div className="grid grid-cols-2 gap-8 relative z-0">
-      <div className="w-[550px] h-[900px] -mt-28 border-r-2 rotate-[3.00deg] bg-gradient-to-r from-[#442143] to-[#2B3057] z-3"></div>
+    <div className="grid grid-cols-2 gap-8 relative z-0 font-berlin p-3 md:p-4 lg:p-0 h-[calc(100vh-60px)]">
+      <div className="w-[600px] h-[900px] -mt-28 border-r-2 rotate-[3.00deg] bg-gradient-to-r from-[#442143] to-[#2B3057] z-3">
+        <div className="mt-20 overflow-hidden">
+          <h3 className="text-center -rotate-[3.00deg]  text-3xl font-bold text-[#DBCBF4]">
+            The Magic Orb
+          </h3>
+          <div className="w-[635px] -rotate-3">
+            <Image
+              src={"https://i.ibb.co/RD05jMX/Tool-label-1.png"}
+              alt="the magic orb"
+              width={1000}
+              height={1000}
+              className="w-full h-full mt-6 -ml-4 object-cover"
+            />
+          </div>
+        </div>
+        <div className="mt-14 px-8 grid grid-cols-2 justify-items-end">
+          <div>
+            <p className="text-[#DBCBF4] -rotate-3">Info</p>
+            <div className="-rotate-3 ml-2">
+              {[1, 2, 3].map((infoItem) => (
+                <p key={infoItem} className="text-[10px] mb-4 text-[#E5BD9D]">
+                  <span className="text-[#DBCBF4]">Spiritual Guidance: </span>
+                  Our experienced team of spiritual practitioners, mentors, and
+                  advisors share their knowledge and expertise to offer guidance
+                  on a wide range of spiritual topics. Through articles, blog
+                  posts, and personalized consultations, we aim to support
+                  individuals in finding answers, gaining new perspectives, and
+                  deepening their understanding _of themselves and the universe.
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-y-10">
+            <div className="border-l-2 pl-8 h-[200px]">
+              <p className="text-[#DBCBF4] -rotate-3 text-center">Activity</p>
+              <div className="-rotate-3 mt-8">
+                {[1, 2, 3, 4].map((activityItem) => (
+                  <div
+                    key={activityItem}
+                    className="flex gap-4 text-[#DBCBF4] items-center justify-center">
+                    <p>Today: 7min</p>
+                    <p>Total: 3.5h</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="-rotate-3 mb-8 relative">
+              <p className="bg-[#BFB1D6] text-[#624652] text-[15px] w-fit px-3 py-1 font-semibold rounded-md cursor-pointer">
+                Use tool
+              </p>
+              <BiSolidChevronsLeft
+                className="absolute -top-4 -left-14"
+                size={25}
+                color="white"
+              />
+              <BiSolidChevronsRight
+                className="absolute -bottom-4 -right-14"
+                size={25}
+                color="white"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="grid grid-cols-2 gap-4">
         {imageCardInfo.map((singleImageCardInfo) => (
           <ImageCard key={singleImageCardInfo.id} {...singleImageCardInfo} />
