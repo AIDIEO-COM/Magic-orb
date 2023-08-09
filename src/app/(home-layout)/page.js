bda3630724a54c5e4a49b7d03a122343e6eeb1fc
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-09 11:58:54 +0600

    home banner added

--- src/app/(home-layout)/page.js
+++ src/app/(home-layout)/page.js
@@ -1,7 +1,8 @@
+import Home from "@/components/pages/home/home";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="font-berlin text-2xl h-screen bg-gradient-to-r from-[#3D2D3B] to-[#232141]">This is home page</div>;
+  return <Home></Home>
 };
 
 export default HomePage;
---
+++ src/components/pages/home/home.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div className="font-berlin  h-screen bg-gradient-to-r from-[#3D2D3B] to-[#232141]">
+      <div className="lg:grid grid-cols-10 h-[60%]">
+        <div className="col-start-1 col-end-9 ">
+          <Image
+            src={"https://i.ibb.co/RD05jMX/Tool-label-1.png"}
+            alt="homepageimg"
+            width={800}
+            height={800}
+            className="w-full h-full"
+          ></Image>
+        </div>
+        <div className="col-start-9 col-end-11 flex flex-col gap-y-3">
+          <div className="h-[30%] w-full relative overflow-hidden">
+            <Image
+              src={"https://i.ibb.co/QN7h6G0/orb22-1.png"}
+              alt="sideimg"
+              width={300}
+              height={250}
+              className="w-full h-full"
+            ></Image>
+            <div className="absolute top-0 left-0 right-0 bottom-0 px-5 py-3">
+              <div>
+                <h1 className="text-[#FFC8AA] text-2xl font-semibold font-berlin">
+                  Magic Orb
+                </h1>
+                <p className="text-[#DBCBF4] font-berlin text-sm">
+                  Talk with your personal assistance
+                </p>
+              </div>
+              <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2  bg-[#6b5863] rounded-xl px-3 ">
+                <p className="text-[#E5BD9D] text-[11px] font-semibold tracking-wider">
+                  Chat Now
+                </p>
+              </div>
+              <div className="absolute bottom-4 left-5 text-[#FFC8AA] text-3xl">
+                0
+              </div>
+              <div className="absolute bottom-4 right-5 text-[#FFC8AA] text-3xl">
+                32
+              </div>
+            </div>
+          </div>
+          <div className="h-[70%] w-full px-2 pb-3 pt-0">
+            <div className="default-gradient rounded-[16px] w-full h-full">
+              <p className="font-berlin text-[#FFC8AA] text-3xl p-6">
+                Adds here for free version. Click to swap to the premium version
+                view
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
