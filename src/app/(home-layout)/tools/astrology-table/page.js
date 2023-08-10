538ecc58d02b520174c087eda7dbbda8fc98e7e3
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-10 17:59:23 +0600

    astrology page initial transition added

--- src/app/(home-layout)/tools/astrology-table/page.js
+++ src/app/(home-layout)/tools/astrology-table/page.js
@@ -1,7 +1,11 @@
+import AstrologyTable from "@/components/pages/astrology-table/AstrologyTable";
+
 
 const page = () => {
     return (
-        <div className="font-berlin p-3 md:p-4 lg:p-0 h-[calc(100vh-60px)]">This is Astrology page</div>
+        <div className="font-berlin p-3 md:p-4 lg:p-0 lg:min-h-[calc(100vh-60px)] relative">
+            <AstrologyTable></AstrologyTable>
+        </div>
     );
 };
 
