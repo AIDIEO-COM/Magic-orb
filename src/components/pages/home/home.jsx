4a24faa220d65793c79aa2497e3276f9d5c7a936
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-10 13:14:37 +0600

    home card responsive done

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -118,11 +118,36 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="overflow-hidden w-full ">
+      <div className={` w-full lg:grid grid-cols-2 md:grid-cols-3 xl:grid-cols-7 gap-x-2 duration-700 ${cardClassName}`}>
         <div
-          className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3  xl:grid-cols-4 gap-y-5 px-2 mt-[25px] duration-700 ${cardClassName}`}
+          className={`flex justify-between lg:col-start-1 lg:col-end-4 mt-[25px] items-center `}
         >
-          {imageCardInfo.slice(0, 4).map((singleImageCardInfo) => (
+          {imageCardInfo.slice(0, 2).map((singleImageCardInfo) => (
+            <ImageCard key={singleImageCardInfo.id} {...singleImageCardInfo} />
+          ))}
+        </div>
+        {/* gift card */}
+        <div className="col-start-4 col-end-5 mt-2 justify-items-center hidden lg:inline-block">
+        <div className="p-5 mt-4 mx-auto  flex flex-col xl:w-40 xl:h-[180px] 2xl:w-44 2xl:h-[250px] items-center justify-between rounded-xl bg-[#221c3de0] shadow-[10px_10px_8px10px#00000024]">
+          <p className="text-[#E5BD9D] text-xs font-semibold text-center">
+            Redeem your gift card
+          </p>
+          <Image
+            src="https://i.ibb.co/GCDyhJw/Default-cosmic-pink-gift-card-0-3507d5d2-e214-4bf6-b126-ff41039a25cf-0-1.png"
+            alt="gift card"
+            width={108}
+            height={148}
+            className="lg:w-[70px] lg:h-[100px] xl:w-[80px] xl:h-[105px] 2xl:w-[108px] 2xl:h-[148px]"
+          />
+          <button className="mx-auto text-[#E5BD9D] text-xs bg-[#674B53] px-2 font-semibold rounded-lg py-1">
+            Take the card
+          </button>
+        </div>
+        </div>
+        <div
+          className={`flex justify-between  lg:col-start-5 lg:col-end-8 mt-[25px] items-center `}
+        >
+          {imageCardInfo.slice(2, 4).map((singleImageCardInfo) => (
             <ImageCard key={singleImageCardInfo.id} {...singleImageCardInfo} />
           ))}
         </div>
