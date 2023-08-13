4887eaa18000ceb11876132f5ce6ee30e4756e07
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-13 20:05:14 +0600

    separate routes data

---
+++ src/routes-data/sidebarRoute/relationalRoute.js
@@ -0,0 +1,19 @@
+const relationalRoute = [
+    {
+      id: "1",
+      routeName: "Policies",
+      href: "/policies",
+    },
+    {
+      id: "2",
+      routeName: "Contact",
+      href: "/contact",
+    },
+    {
+      id: "3",
+      routeName: "About us",
+      href: "/about-us",
+    }
+  ];
+
+export default relationalRoute;
