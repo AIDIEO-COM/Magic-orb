56e8803c7ee68e060bd9bc253337dab5bf508c3a
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-14 15:19:42 +0600

    destiny page responsive done

--- src/components/pages/discoverDestiny/cardContainer/CardContainer.jsx
+++ src/components/pages/discoverDestiny/cardContainer/CardContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CardContainer = ({selectRandomCards, genarateCards, setCard}) => {
     return (
-        <div className={`w-[85%] sm:w-[60%] md:w-[27%] xl:w-[25%]  h-[500px] rounded-[50px] p-4  text-[#FFC8AA] lg:flex flex-col  bg-opacity-60 ${selectRandomCards && 'hidden'}`}>
+        <div className={`w-[85%] sm:w-[60%] md:w-[27%] xl:w-[25%]  h-[500px] rounded-[50px] p-4  text-[#FFC8AA] lg:flex flex-col  bg-opacity-60 ${selectRandomCards && 'md:hidden'}`}>
           <h1 className="text-base lg:text-3xl font-berlin text-center pb-4">
             Cards
           </h1>
--- src/components/pages/discoverDestiny/predictCards/PredictCards.jsx
+++ src/components/pages/discoverDestiny/predictCards/PredictCards.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const PredictCards = ({ togglePredictDetails, showPredictDetails, selectedCards }) => {
-  console.log(selectedCards)
   let genarateCards = [1, 2, 3, 4, 5, 6, 7];
   const [btnTransition, setBtnTransition] = useState("-translate-y-[120px] ");
   const [cardOpacity, setCardOpacity] = useState("opacity-0");
@@ -18,9 +17,9 @@ const PredictCards = ({ togglePredictDetails, showPredictDetails, selectedCards 
     return () => clearTimeout(timeOut);
   }, []);
   return (
-    <div className="relative pb-[70px] w-fit h-fit">
+    <div className="relative pb-[70px] w-full h-fit">
       <div
-        className={`grid grid-cols-3 gap-y-3 justify-items-center lg:flex  gap-x-3 ${cardOpacity} duration-700 w-full`}
+        className={`grid grid-cols-3 gap-y-3 justify-items-center lg:flex justify-center  gap-x-3 ${cardOpacity} duration-700 w-full`}
       >
         {genarateCards.map((index) => (
           <Image
@@ -29,7 +28,7 @@ const PredictCards = ({ togglePredictDetails, showPredictDetails, selectedCards 
             alt="elipsis"
             width={100}
             height={100}
-            className="md:w-full md:h-full w-[80px] h-[100px]"
+            className="w-[80px] h-[100px] lg:w-[40px] lg:h-[60px]"
           ></Image>
         ))}
       </div>
