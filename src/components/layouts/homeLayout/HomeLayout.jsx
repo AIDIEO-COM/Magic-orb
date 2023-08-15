3b5912fde0173c080418a5a7309ebdbaeb15a3df
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-16 02:53:36 +0600

    conflicts resolved

--- src/components/layouts/homeLayout/HomeLayout.jsx
+++ src/components/layouts/homeLayout/HomeLayout.jsx
@@ -1,39 +1,44 @@
 "use client";
-import React, { useState } from "react";
-import Sidebar from "../sidebar/Sidebar";
-import Navbar from "../navbar/Navbar";
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../shared/sidebar/Sidebar";
+import Navbar from "../../shared/navbar/Navbar";
+import { usePathname } from "next/navigation";
 
 const HomeLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
+  const currentPath = usePathname();
+  useEffect(() => {
+    if (currentPath === currentPath) {
+      setSidebarOpen(false);
+    }
+  }, [currentPath]);
   return (
-    <div className="xl:grid grid-cols-8  relative bg-gradient-to-b from-[#442143] to-[#2B3057]">
+    <div className="flex relative bg-gradient-to-b from-[#442143] to-[#2B3057] min-h-screen lg:h-screen lg:w-screen ">
       {/* mobile side drawer */}
       <div className="overflow-hidden h-fit w-fit xl:hidden">
         <div
           className={`h-screen overflow-y-scroll xl:hidden bg-gradient-to-b from-[#3D2D3B] to-[#232141] fixed top-0 left-0 ${
             sidebarOpen ? "" : "-translate-x-[100%]"
-          } w-[60%] sm:w-[300px] z-50 duration-500`}
-        >
+          } w-[60%] sm:w-[300px] z-50 duration-500`}>
           <Sidebar></Sidebar>
         </div>
         {sidebarOpen && (
           <button
             onClick={toggleSidebar}
-            className="absolute top-0 right-0 left-0 bottom-0 bg-black bg-opacity-30 z-[49]"
-          ></button>
+            className="absolute top-0 right-0 left-0 bottom-0 bg-black bg-opacity-30 z-[49]"></button>
         )}
       </div>
       {/* desktop sidebar */}
-      <div className="hidden xl:inline-block lg:col-start-1 lg:col-end-2 bg-gradient-to-b from-[#442143] to-[#2B3057] border-r-2 border-white ">
+      <div className="hidden xl:inline-block w-[220px] border-r-2 border-white h-full">
         <Sidebar></Sidebar>
       </div>
-      <div className="xl:col-start-2 xl:col-end-9">
+      <div className="w-full h-full  overflow-hidden relative">
         <Navbar toggleSidebar={toggleSidebar}></Navbar>
-        <div className="lg:p-8 lg:pb-0 w-full  bg-gradient-to-r from-[#3D2D3B] to-[#232141]">
-        {children}
+        <div className="lg:pt-[25px] lg:pb-[40px] lg:px-[32px] xl:pb-[45px] xl:px-[42px] w-full min-h-screen h-full default-background-color">
+          {children}
         </div>
       </div>
     </div>
