fa78efd12b314e6064cdce0e96703360a1c5979c
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-16 01:55:14 +0600

    logo linked

--- src/components/shared/sidebar/sidebar.jsx
+++ src/components/shared/sidebar/sidebar.jsx
@@ -8,13 +8,15 @@ const Sidebar = () => {
   return (
     <div className="font-berlin relative h-full">
       <div className="pt-5">
-        <Image
-          width={120}
-          height={51}
-          src="https://i.ibb.co/kxxsFW7/home-modified-3.png"
-          alt="magic orb logo"
-          className="mx-auto"
-        />
+        <Link href={"/"}>
+          <Image
+            width={140}
+            height={51}
+            src="https://i.ibb.co/kxxsFW7/home-modified-3.png"
+            alt="magic orb logo"
+            className="mx-auto"
+          />
+        </Link>
       </div>
       <div className="mt-8 flex flex-col gap-y-3 2xl:gap-y-[30px]">
         {sidebarRoute.map((singleRoute) => (
@@ -23,7 +25,8 @@ const Sidebar = () => {
             exact={singleRoute.href === "/"}
             activeClassName="bg-gradient-to-b from-[#281B32] to-[#2d1e38f4] duration-500 transition-all"
             href={singleRoute.href}
-            className="px-4 py-2 mx-2 2xl:mx-4 rounded-lg ">
+            className="px-4 py-2 mx-2 2xl:mx-4 rounded-lg "
+          >
             <div className="flex items-center gap-[10px] ">
               <Image
                 src={singleRoute.iconLink}
@@ -44,7 +47,8 @@ const Sidebar = () => {
           {relationalRoute.map((singleRoute) => (
             <li
               key={singleRoute.id}
-              className="mb-1 mx-6 text-white font-berlin">
+              className="mb-1 mx-6 text-white font-berlin"
+            >
               <Link href={singleRoute.href}>{singleRoute.routeName}</Link>
             </li>
           ))}
