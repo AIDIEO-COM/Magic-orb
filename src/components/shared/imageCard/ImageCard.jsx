7735d755b8b5fe1289ce3229d33b0294cfe8b897
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-08-17 10:07:01 +0600

    fix button text width issue

--- src/components/shared/imageCard/ImageCard.jsx
+++ src/components/shared/imageCard/ImageCard.jsx
@@ -50,7 +50,7 @@ const ImageCard = ({
             {try_count}
           </div>
           <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2  bg-[#6b5863] rounded-xl px-3 py-1 hidden md:inline-block">
-            <p className="text-[#E5BD9D] font-berlin text-[11px] lg:font-normal tracking-wider text-center ">
+            <p className="text-[#E5BD9D] font-berlin text-[12px] lg:font-normal tracking-wider text-center ">
               {game_name}
             </p>
           </div>
