56d399fb9be76d89050b91d5cd46021b1c42a918
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-18 11:57:01 +0600

    account page issue solved

--- src/app/(home-layout)/account/page.js
+++ src/app/(home-layout)/account/page.js
@@ -1,5 +1,11 @@
 import FirstSection from "@/components/pages/account/FirstSection";
 import GrapSection from "@/components/pages/account/GrapSection";
+import Personality from "@/components/pages/account/activity&Personality/Personality";
+import DailyTask from "@/components/pages/account/dailyTask/DailyTask";
+import Profile from "@/components/pages/account/profile/Profile";
+import Statistics from "@/components/pages/account/statistics/Statistics";
+import Subscriptions from "@/components/pages/account/subscriptions/Subscriptions";
+import GiftCard from "@/components/shared/giftCard/GiftCard";
 
 const subscriptionModel = [
   {
@@ -27,12 +33,28 @@ const subscriptionModel = [
 
 const Account = () => {
   return (
-      <div className="font-berlin p-3 md:p-4 lg:p-0 h-full w-full flex items-center min-h-[calc(100vh-100px)]">
-        <div className="">
-          <FirstSection subscriptionModel={subscriptionModel}></FirstSection>
-          <GrapSection></GrapSection>
+    <div className="font-berlin p-3 md:p-4 lg:p-0 h-full w-full flex items-center min-h-[calc(100vh-100px)]">
+      <div className="lg:grid grid-cols-1 lg:grid-cols-6 lg:gap-x-[18px] xl:gap-x-[28px] w-full gap-y-10 lg:gap-y-[23px] h-fit">
+        <div className="w-full lg:col-start-1 lg:col-end-5 lg:h-[257px] flex gap-x-2 md:gap-x-3 lg:gap-x-[18px] xl:gap-x-[28px]">
+          <DailyTask></DailyTask>
+          <GiftCard></GiftCard>
+          <span className="hidden md:inline-block h-full"><Profile></Profile></span>
+        </div>
+        <div className="md:hidden mt-4">
+          <Profile></Profile>
+        </div>
+        <div className=" w-full lg:h-[257px] lg:col-start-5 lg:col-end-7 mt-5 lg:mt-0">
+          <Subscriptions subscriptionModel={subscriptionModel}></Subscriptions>
+        </div>
+        <div className=" w-full  lg:col-start-1 lg:col-end-5 lg:h-[257px] flex lg:gap-x-[18px] xl:gap-x-[28px] mt-5 lg:mt-0">
+          <Personality></Personality>
+          <div className="account-box-gradient default-shadow hidden md:flex items-center justify-between rounded-xl grow-0 lg:w-[44%]"></div>
+        </div>
+        <div className=" w-full lg:col-start-5 lg:col-end-7 lg:h-[257px] mt-5 lg:mt-0">
+          <Statistics></Statistics>
         </div>
       </div>
+    </div>
   );
 };
 
