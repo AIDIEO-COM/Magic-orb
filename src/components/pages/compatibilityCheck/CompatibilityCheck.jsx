61d02dad5178df35fd686b7cfd7ff4a3dac1b8b2
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-22 00:08:31 +0600

    add person btn working

--- src/components/pages/compatibilityCheck/CompatibilityCheck.jsx
+++ src/components/pages/compatibilityCheck/CompatibilityCheck.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import CompatibilityContent from "./CompatibilityContent";
+import CompatibilityContent from "./compatibilityContent/CompatibilityContent";
 import { useEffect, useState } from "react";
 
 const CompatibilityCheck = () => {
@@ -8,6 +8,29 @@ const CompatibilityCheck = () => {
     "w-[400px] h-[300px] bottom-0 left-[300px]"
   );
   const [secondBgImg, setSecondBgImg] = useState("opacity-0");
+  const [isCheckSoulmatesClicked, setIsCheckSoulmatesClicked] = useState(false);
+  const [isPersonSoulmates, setisPersonSoulmates] = useState([]);
+  // set person soulmates
+  const setPersonSoulmates = () => {
+    if (isPersonSoulmates.length === 2) {
+      return;
+    } else {
+      setisPersonSoulmates((prev) => [...prev, 1]);
+    }
+  };
+  // check soulmates btn toggle
+  const checkSoulmatesBtnToggle = () => {
+    if (isPersonSoulmates.length === 2) {
+      setIsCheckSoulmatesClicked(true);
+    } else {
+      return;
+    }
+  };
+  // control try again btn 
+  const tryAgainBtnToggle = () => {
+    setIsCheckSoulmatesClicked(false);
+    setisPersonSoulmates([]);
+  }
   useEffect(() => {
     const timeOut = setTimeout(() => {
       setFirstTransitionImg("bottom-0 left-0 w-full h-full");
@@ -22,25 +45,54 @@ const CompatibilityCheck = () => {
         alt="img"
         width={1000}
         height={1000}
-        className={`${firstTransitionImg} duration-700 lg:absolute rounded-[15px] min-h-[230px]`}></Image>
+        className={`${firstTransitionImg} duration-700 lg:absolute rounded-[15px] min-h-[230px]`}
+      ></Image>
       <div
-        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 lg:flex xl:gap-12 2xl:gap-24 hidden ${secondBgImg} duration-700`}>
-        <Image
-          src={"https://i.ibb.co/7WQWsnk/LEft.png"}
-          alt="img"
-          width={400}
-          height={400}
-          className="w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px]"></Image>
-        <Image
-          src={"https://i.ibb.co/dM41vcy/Roght.png"}
-          alt="img"
-          width={400}
-          height={400}
-          className="w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px]"></Image>
+        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 lg:flex xl:gap-12 2xl:gap-24 hidden ${secondBgImg} duration-700 `}
+      >
+        <div className="lg:flex xl:gap-12 2xl:gap-24 relative">
+          <Image
+            src={"https://i.ibb.co/7WQWsnk/LEft.png"}
+            alt="img"
+            width={400}
+            height={400}
+            className={`w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px] ${
+              isCheckSoulmatesClicked &&
+              "translate-x-1/2 duration-500 opacity-0"
+            }`}
+          ></Image>
+          {/* this img show when check soulmates btn clicked */}
+          <Image
+            src={"https://i.ibb.co/S0Vr8gD/Merged.png"}
+            alt="img"
+            width={489}
+            height={469}
+            className={`absolute left-[56%] -top-[10%] w-[250px] h-[250px] 2xl:w-[489px] 2xl:h-[469px] -translate-x-[60%] duration-500 ${
+              isCheckSoulmatesClicked ? "opacity-100" : "opacity-0"
+            }`}
+          ></Image>
+          <Image
+            src={"https://i.ibb.co/dM41vcy/Roght.png"}
+            alt="img"
+            width={400}
+            height={400}
+            className={`w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px] ${
+              isCheckSoulmatesClicked &&
+              "-translate-x-1/2 duration-500 opacity-0"
+            }`}
+          ></Image>
+        </div>
       </div>
       <div
-        className={`lg:absolute lg:top-0 right-0 left-0 bottom-0 w-full h-full duration-1000 ${secondBgImg} `}>
-        <CompatibilityContent></CompatibilityContent>
+        className={`lg:absolute lg:top-0 right-0 left-0 bottom-0 w-full h-full duration-1000 ${secondBgImg} `}
+      >
+        <CompatibilityContent
+          isPersonSoulmates={isPersonSoulmates}
+          setPersonSoulmates={setPersonSoulmates}
+          checkSoulmatesBtnToggle={checkSoulmatesBtnToggle}
+          isCheckSoulmatesClicked={isCheckSoulmatesClicked}
+          tryAgainBtnToggle={tryAgainBtnToggle}
+        ></CompatibilityContent>
       </div>
     </div>
   );
---
+++ src/components/pages/compatibilityCheck/compatibilityContent/selectSaved&Friends/SelectSavedFriends.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import React from 'react';
+
+const SelectSavedFriends = ({genarateIcon}) => {
+    return (
+        <div className="w-[85%] sm:w-[60%] md:w-[27%] xl:w-[20%] min-h-[300px] md:min-h-[80%]  default-shadow bg-[#20192A]  rounded-[50px] py-4 text-[#FFC8AA] bg-opacity-60">
+          <h1 className="text-base lg:text-2xl font-berlin text-center px-3">
+            Select saved & friends
+          </h1>
+          <div className="flex gap-2 flex-wrap pt-7 justify-center px-0.5">
+            {genarateIcon.slice(0, 10).map((index) => (
+              <div
+                key={index}
+                className="w-[45px] h-[45px] bg-[#88848D] rounded-full flex justify-center items-center">
+                <Image
+                  key={index}
+                  src={"https://i.ibb.co/swDCL0H/libra-modified-1.png"}
+                  alt="libraImg"
+                  width={30}
+                  height={30}></Image>
+              </div>
+            ))}
+          </div>
+        </div>
+    );
+};
+
+export default SelectSavedFriends;
