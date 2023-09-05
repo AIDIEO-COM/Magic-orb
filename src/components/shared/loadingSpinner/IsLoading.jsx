765d7898c059460317ee5d859cd43f4142abc3d6
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-05 19:58:32 +0600

    loader spinner modified

--- src/components/shared/loadingSpinner/IsLoading.jsx
+++ src/components/shared/loadingSpinner/IsLoading.jsx
@@ -3,7 +3,7 @@ import "./isLoading.css";
 const IsLoading = () => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
     </div>
   );
 };
