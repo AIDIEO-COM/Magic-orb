1d671bd7d84e418d84040b267fc640f504aeae83
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-10 12:33:18 +0600

    planet position  page 2 added

--- src/components/pages/planet-positioning/solarSystemTwo/PlanetPositioningTwo.jsx
+++ src/components/pages/planet-positioning/solarSystemTwo/PlanetPositioningTwo.jsx
@@ -11,7 +11,6 @@ import venus from "/public/planet-images/venus.png";
 import mars from "/public/planet-images/mars.png";
 import mercury from "/public/planet-images/mercury.png";
 import Link from "next/link";
-import ellipsePlanet from "/public/ellipse-planet.svg";
 
 const PlanetPositioningTwo = () => {
   return (
@@ -22,18 +21,82 @@ const PlanetPositioningTwo = () => {
         </button>
       </Link>
       <div className="h-full w-full flex justify-center items-center ">
-        <div className="relative border w-fit h-fit">
+        <div className="relative w-fit h-fit flex justify-center items-center">
           <Image
-            src={"https://i.ibb.co/BVgY71J/Rectangle-53-1.png"}
+            src={"https://i.ibb.co/YZtQRY0/galaxy.png"}
             alt="planet position two"
             width={700}
             height={500}
             className=""
           ></Image>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-[55%] -translate-y-1/2 w-[80%] border h-[80%] rounded-full flex justify-center items-center">
-            <Image src={sun} alt="ellipse planet" width={97} height={97} className=""></Image>
-            {/* <Image src={ellipsePlanet} alt="ellipse planet" width={97} height={97}></Image>
-            <Image src={ellipsePlanet} alt="ellipse planet" width={97} height={97}></Image> */}
+          <div className="absolute top-0 right-0 left-0 bottom-0 ">
+            <div className="flex justify-center items-center h-full relative ">
+              <div className=" absolute bottom-[25.2%] left-0  w-[61.3%] h-fit -rotate-[50deg] flex items-center pl-[10%]">
+                <Image
+                  src={neptune}
+                  alt="ellipse planet"
+                  width={20}
+                  height={21}
+                  className=""
+                ></Image>
+                <Image
+                  src={uranus}
+                  alt="ellipse planet"
+                  width={31}
+                  height={23}
+                  className=""
+                ></Image>
+                <Image
+                  src={saturn}
+                  alt="ellipse planet"
+                  width={45}
+                  height={34}
+                  className=""
+                ></Image>
+                <Image
+                  src={jupiter}
+                  alt="ellipse planet"
+                  width={76}
+                  height={42}
+                  className=""
+                ></Image>
+                <Image
+                  src={venus}
+                  alt="ellipse planet"
+                  width={34}
+                  height={19}
+                  className=""
+                ></Image>
+                <Image
+                  src={earth}
+                  alt="ellipse planet"
+                  width={25}
+                  height={25}
+                  className=""
+                ></Image>
+                <Image
+                  src={mercury}
+                  alt="ellipse planet"
+                  width={80}
+                  height={80}
+                  className=""
+                ></Image>
+                <Image
+                  src={mars}
+                  alt="ellipse planet"
+                  width={42}
+                  height={23}
+                  className=""
+                ></Image>
+                <Image
+                  src={sun}
+                  alt="ellipse planet"
+                  width={76}
+                  height={76}
+                  className=""
+                ></Image>
+              </div>
+            </div>
           </div>
         </div>
       </div>
