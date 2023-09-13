11dbf4f38e7a425d16f82092fc523e54dafcf6d8
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-13 12:09:55 +0600

    home banner replace with hd

--- src/components/pages/home/bannerSideContent/MagicOrbChatCard.jsx
+++ src/components/pages/home/bannerSideContent/MagicOrbChatCard.jsx
@@ -1,10 +1,18 @@
+"use client"
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BannerContent from '../BannerContent';
 
-const MagicOrbChatCard = ({magicOrbChatAnimate}) => {
+const MagicOrbChatCard = () => {
+  const [magicOrbChatAnimate, setMagicOrbChatAnimate] = useState("opacity-0 duration-1000");
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setMagicOrbChatAnimate("opacity-100 duration-1000");
+    }, 50);
+    return () => clearTimeout(timeOut);
+  });
     return (
-        <div className={`absolute -left-[4.5%] right-[2.8%] top-0 h-fit  z-1 overflow-hidden hidden lg:inline-block ${magicOrbChatAnimate} duration-1000 default-gradient rounded-r-[16px]`}>
+        <div className={`absolute -left-[12%] 2xl:-left-[9.5%] right-[3%] top-0 h-fit  z-1 overflow-hidden hidden lg:inline-block ${magicOrbChatAnimate} transition-all default-gradient rounded-r-[16px] `}>
         <Image
           src={"/thumbnails/chat-cart-bg.png"}
           alt="sideimg"
