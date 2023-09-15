65fc47e5f3be02e40354743e4d61aa477f19ae9f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-15 15:24:14 +0600

    dreams feed wrapp by animation

--- src/app/(home-layout)/tools/dream-interpretation/dreams-feed/page.js
+++ src/app/(home-layout)/tools/dream-interpretation/dreams-feed/page.js
@@ -1,12 +1,15 @@
 import DreamsFeed from '@/components/pages/dreams-feed/DreamsFeed';
+import PageWrapper from '@/components/transitions/pageWrapper/PageWrapper';
 import SecureRoute from '@/utls/SecureRoute';
 
 const page = () => {
     return (
         <SecureRoute>
-            <div className='font-berlin p-3 md:p-4 lg:p-0 min-h-[calc(100vh-100px)] h-full bg-[#3A284C] relative w-full  rounded-[15px] default-shadow '>
-                <DreamsFeed></DreamsFeed>
-            </div>
+            <PageWrapper>
+                <div className='font-berlin p-3 md:p-4 lg:p-0 min-h-[calc(100vh-100px)] h-full bg-[#3A284C] relative w-full  rounded-[15px] default-shadow '>
+                    <DreamsFeed></DreamsFeed>
+                </div>
+            </PageWrapper>
         </SecureRoute>
     );
 };
